08969a2e8e3984fdf3167e8a93cd1a258ee5d755
= <=>
2019-05-08 00:31:48 -0700

    please have mercy

--- server/index.js
+++ server/index.js
@@ -114,7 +114,7 @@ app.get("/store", storeController.getStore);
 //For Stripe payments
 app.post("/charge", authController.purchasePremium);
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
