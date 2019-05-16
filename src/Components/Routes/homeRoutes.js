b9b3909efc03b765da9b5433cec3d68808a4f1ae
= <=>
2019-05-16 10:59:22 -0700

    I goofed up

--- src/Components/Routes/homeRoutes.js
+++ src/Components/Routes/homeRoutes.js
@@ -4,13 +4,10 @@ import Brewery from '../Brewery/Brewery';
 import Breweries from '../Breweries/Breweries'
 import Store from '../Store/Store';
 import Notifications from '../Notifications/Notifications';
-import Homepage from '../Homepage/Homepage';
-import Profile from '../Profile/Profile';
 
 export default (
     <Switch>
-        <Route exact path='/home' component={Homepage}/>
-        <Route path='/users/:id' component={Profile}/>
+        <Route exact path='/home' component={Breweries}/>
         <Route path = '/breweries/:id' component = {Brewery} /> 
         <Route path = '/notifications' component = {Notifications} /> 
         <Route path = '/store' component = {Store} /> 
